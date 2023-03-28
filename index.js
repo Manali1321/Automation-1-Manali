6aabf68fd23a850523c883277a7f2fe3e509a408
manali1321 <patelmanali216@gmail.com>
2023-03-28 17:49:50 -0400

    validation and store input value

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 window.onload = submitForm;
-var LetterRange = /^[A-Za-z]+$/;
 
 var formHandle = document.forms.form;
 var form__field = formHandle.form__field;
@@ -16,6 +15,11 @@ var form__testcase_error = document.getElementById("form__testcase_error");
 
 var form__spec = formHandle.form__spec;
 var form__spec_error = document.getElementById("form__spec_error");
+
+var LetterRange = /^[A-Za-z]+$/;
+var LetterAndNum = /^[A-Za-z0-9]+$/;
+var RegxForRegx = /^[a-zA-Z0-9!@#$%^&*()_+\-={}[\]\\|:;"'<>,.?\/]+$/;
+
 
 function submitForm() {
   formHandle.onsubmit = function (event) {
@@ -46,17 +50,17 @@ function submitForm() {
       return false;
     }
 
-    if (!LetterRange.test(form__pattern.value)) {
+    if (!LetterAndNum.test(form__pattern.value)) {
       form__pattern_error.style.background = "red";
       form__pattern_error.style.color = "white";
-      form__pattern_error.innerHTML = "*Please enter your field name in letters only!";
+      form__pattern_error.innerHTML = "*Please enter RegEX(Example: /^[A-Za-z]+$/) pattern only!";
       form__pattern.focus();
       return false;
     }
-    if (!LetterRange.test(form__testtype.value)) {
+    if (form__testtype.value === '--Choose--') {
       form__testtype_error.style.background = "red";
       form__testtype_error.style.color = "white";
-      form__testtype_error.innerHTML = "*Please enter your field name in letters only!";
+      form__testtype_error.innerHTML = "*Please select type of test you are intrested!";
       form__testtype.focus();
       return false;
     }
@@ -86,49 +90,3 @@ function submitForm() {
   }
 
 }
-
-// // get form and input elements
-// const form = document.getElementById('form');
-// const fieldInput = document.getElementById('form__field');
-// const patternInput = document.getElementById('form__pattern');
-// const typeSelect = document.getElementById('form__testtype');
-// const numTestCasesInput = document.getElementById('form__testcase');
-// const numSpecsInput = document.getElementById('form__spec');
-// // add event listener for form submit
-// form.addEventListener('submit', (event) => {
-//   console.log(fieldInput.value)
-//   // prevent default form submission behavior
-//   event.preventDefault();
-
-//   // validate input values
-//   let isValid = true;
-//   if (!fieldInput.value) {
-//     document.getElementById('form__field_error').textContent = 'Please enter a field name';
-//     isValid = false;
-//   }
-//   if (!patternInput.value) {
-//     document.getElementById('form__pattern_error').textContent = 'Please enter a regular expression pattern';
-//     isValid = false;
-//   }
-//   if (typeSelect.value === '--Choose--') {
-//     document.getElementById('form__testtype_error').textContent = 'Please select a test type';
-//     isValid = false;
-//   }
-//   if (!numTestCasesInput.value || isNaN(numTestCasesInput.value)) {
-//     document.getElementById('form__testcase_error').textContent = 'Please enter a valid number of test cases';
-//     isValid = false;
-//   }
-//   if (!numSpecsInput.value || isNaN(numSpecsInput.value)) {
-//     document.getElementById('form__spec_error').textContent = 'Please enter a valid number of specs';
-//     isValid = false;
-//   }
-
-//   // log input values to console if valid
-//   if (isValid) {
-//     console.log('Field:', fieldInput.value);
-//     console.log('Pattern:', patternInput.value);
-//     console.log('Test type:', typeSelect.value);
-//     console.log('Number of test cases:', numTestCasesInput.value);
-//     console.log('Number of specs:', numSpecsInput.value);
-//   }
-// });
